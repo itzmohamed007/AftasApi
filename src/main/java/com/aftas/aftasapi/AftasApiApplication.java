df5ff1f3e13a9e31ba80646e845b452f7e281477
= <itzmohamed007@gmail.com>
2023-12-09 17:49:20 +0100

    init

---
+++ src/main/java/com/aftas/aftasapi/AftasApiApplication.java
@@ -0,0 +1,13 @@
+package com.aftas.aftasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftasApiApplication.class, args);
+    }
+
+}
