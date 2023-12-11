05251cccc037c3373539377a3f1d38ad0de2aea1
= <itzmohamed007@gmail.com>
2023-12-11 16:52:48 +0100

    corrected minor error in global controller

--- src/main/java/com/aftas/aftasapi/controllers/FishController.java
+++ src/main/java/com/aftas/aftasapi/controllers/FishController.java
@@ -9,10 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -24,7 +21,8 @@ public class FishController implements GlobalController<ReqFish, ResFish, String
     private final FishService service;
 
     @Override
-    public ResponseEntity<ReqFish> read(String s) {
+    @GetMapping()
+    public ResponseEntity<ResFish> read(String s) {
         return null;
     }
 
--- src/main/java/com/aftas/aftasapi/controllers/GlobalController.java
+++ src/main/java/com/aftas/aftasapi/controllers/GlobalController.java
@@ -13,16 +13,10 @@ import java.util.Map;
 // Y => response dto
 // Z => primary key type
 public interface GlobalController<X, Y, Z> {
-    @GetMapping("/{z}")
-    public ResponseEntity<X> read(@PathVariable Z z);
-    @GetMapping("/all")
+    public ResponseEntity<Y> read(@PathVariable Z z);
     public ResponseEntity<List<Y>> readAll();
-    @GetMapping
     public ResponseEntity<Page<Y>> readAllPaginated(Pageable pageable);
-    @PostMapping
     public ResponseEntity<Y> create(@Valid @RequestBody X x);
-    @PutMapping("/{z}")
-    public ResponseEntity<Y> update(@Valid @RequestBody X x, @PathVariable Z z);
-    @DeleteMapping("{z}")
+    public ResponseEntity<Y> update(X x, Z z);
     public ResponseEntity<Map<String, String>> delete(@PathVariable Z z);
 }
