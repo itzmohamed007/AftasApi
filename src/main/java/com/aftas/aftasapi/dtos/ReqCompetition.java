822f2295c16692e5a5f137eaf14723ee67ee52d5
= <itzmohamed007@gmail.com>
2023-12-12 11:29:23 +0100

    enhanced competition request dto validation

--- src/main/java/com/aftas/aftasapi/dtos/ReqCompetition.java
+++ src/main/java/com/aftas/aftasapi/dtos/ReqCompetition.java
@@ -2,11 +2,13 @@ package com.aftas.aftasapi.dtos;
 
 import com.aftas.aftasapi.models.Hunting;
 import com.aftas.aftasapi.models.Ranking;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -17,18 +19,22 @@ import java.util.List;
 public class ReqCompetition {
     @NotNull(message = "code cannot be null")
     private String code;
+    @JsonFormat(pattern = "dd-MM-yyyy")
     @NotNull(message = "date cannot be null")
-    private LocalDate date;
+    @Pattern(regexp = "^\\d{2}-\\d{2}-\\d{4}$", message = "Invalid date format. Use dd-MM-yyyy")
+    private String date;
     @NotNull(message = "start time cannot be null")
-    private LocalTime startTime;
+    @Pattern(regexp = "^([01]\\d|2[0-3]):([0-5]\\d)$", message = "Invalid time format. Use HH:mm")
+    private String startTime;
     @NotNull(message = "end time cannot be null")
-    private LocalTime endTime;
+    @Pattern(regexp = "^([01]\\d|2[0-3]):([0-5]\\d)$", message = "Invalid time format. Use HH:mm")
+    private String endTime;
     @Min(value = 10, message = "number of participants cannot be inferior than 10")
-    @NotBlank(message = "number of participants cannot be blank")
+    @NotNull(message = "number of participants cannot be null")
     private Integer numberOfParticipants;
     @NotNull(message = "location cannot be null")
     private String location;
     @Min(value = 100, message = "amount cannot be inferior than 100")
-    @NotBlank(message = "amount cannot be blank")
+    @NotNull(message = "amount cannot be null")
     private Double amount;
 }
