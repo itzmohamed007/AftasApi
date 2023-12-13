452d35df7eb22f9592eb85c3c784da317efff32e
= <itzmohamed007@gmail.com>
2023-12-13 10:58:24 +0100

    added ranking repository + added repository annotation to level repository

--- src/main/java/com/aftas/aftasapi/repositories/RankingRepository.java
+++ src/main/java/com/aftas/aftasapi/repositories/RankingRepository.java
@@ -1,10 +1,10 @@
 package com.aftas.aftasapi.repositories;
 
-import com.aftas.aftasapi.models.Level;
+import com.aftas.aftasapi.models.Ranking;
+import com.aftas.aftasapi.models.RankingId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LevelRepository extends JpaRepository<Level, Integer> {
-
+public interface RankingRepository extends JpaRepository<Ranking, RankingId> {
 }
