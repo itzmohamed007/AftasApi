e5ea91274add3361ecfa83122f4940505feb7bb1
= <itzmohamed007@gmail.com>
2023-12-17 01:11:53 +0100

    added ranking calculation method in service and repostiory

--- src/main/java/com/aftas/aftasapi/repositories/RankingRepository.java
+++ src/main/java/com/aftas/aftasapi/repositories/RankingRepository.java
@@ -1,10 +1,14 @@
 package com.aftas.aftasapi.repositories;
 
+import com.aftas.aftasapi.models.Competition;
 import com.aftas.aftasapi.models.Ranking;
 import com.aftas.aftasapi.models.RankingId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RankingRepository extends JpaRepository<Ranking, RankingId> {
+    List<Ranking> findAllByCompetitionOrderByScoreDesc(Competition competition);
 }
--- src/main/java/com/aftas/aftasapi/services/IRankingService.java
+++ src/main/java/com/aftas/aftasapi/services/IRankingService.java
@@ -2,8 +2,12 @@ package com.aftas.aftasapi.services;
 
 import com.aftas.aftasapi.dtos.ReqRanking;
 import com.aftas.aftasapi.dtos.ResRanking;
+import com.aftas.aftasapi.models.Competition;
+import com.aftas.aftasapi.models.Ranking;
 import com.aftas.aftasapi.models.RankingId;
 
-public interface IRankingService extends MainService<ReqRanking, ResRanking, RankingId>{
+import java.util.List;
 
+public interface IRankingService extends MainService<ReqRanking, ResRanking, RankingId>{
+    List<Ranking> calcRanking(Competition competition);
 }
