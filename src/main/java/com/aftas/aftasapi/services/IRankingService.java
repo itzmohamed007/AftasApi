bd9215b62c4937c2c20447fe8360d33971574cab
= <itzmohamed007@gmail.com>
2023-12-17 01:26:07 +0100

    compeleted podium calculation method

--- src/main/java/com/aftas/aftasapi/services/IRankingService.java
+++ src/main/java/com/aftas/aftasapi/services/IRankingService.java
@@ -1,6 +1,7 @@
 package com.aftas.aftasapi.services;
 
 import com.aftas.aftasapi.dtos.ReqRanking;
+import com.aftas.aftasapi.dtos.ResHunting;
 import com.aftas.aftasapi.dtos.ResRanking;
 import com.aftas.aftasapi.models.Competition;
 import com.aftas.aftasapi.models.Ranking;
@@ -9,5 +10,5 @@ import com.aftas.aftasapi.models.RankingId;
 import java.util.List;
 
 public interface IRankingService extends MainService<ReqRanking, ResRanking, RankingId>{
-    List<Ranking> calcRanking(Competition competition);
+    List<ResRanking> calcRanking(Competition competition);
 }
