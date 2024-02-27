9692e5143857846d078fd262bb1c5c9b06459440
= <itzmohamed007@gmail.com>
2024-02-27 01:37:17 +0100

    Added main security beans

--- src/main/java/com/aftas/aftasapi/config/AppConfig.java
+++ src/main/java/com/aftas/aftasapi/config/AppConfig.java
@@ -1,13 +1,43 @@
 package com.aftas.aftasapi.config;
 
+import com.aftas.aftasapi.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
+    private final UserRepository repository;
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findUserByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("User not found")
+        );
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userDetailsService());
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
     }
 }
