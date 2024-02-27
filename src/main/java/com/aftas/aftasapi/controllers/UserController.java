cc5b69fab80f46986ce45d996fb417406d81138a
= <itzmohamed007@gmail.com>
2024-02-27 03:37:50 +0100

    added unlocking accounts feature

--- src/main/java/com/aftas/aftasapi/controllers/UserController.java
+++ src/main/java/com/aftas/aftasapi/controllers/UserController.java
@@ -6,11 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("api/users")
@@ -19,8 +18,19 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
+    @GetMapping
+    public ResponseEntity<List<User>> fetchAllAccounts() {
+        return new ResponseEntity<>(userService.fetchAllAccounts(), HttpStatus.OK);
+    }
+
     @GetMapping("/locked")
     public ResponseEntity<List<User>> fetchAllLockedAccounts() {
         return new ResponseEntity<>(userService.fetchAllLockedAccounts(), HttpStatus.OK);
+    }
+
+    @PatchMapping("unlock/{num}")
+    public ResponseEntity<?> unlockAccount(@PathVariable Integer num) {
+        userService.unlockAccount(num);
+        return new ResponseEntity<>(Map.of("message", "Account updated successfully"), HttpStatus.OK);
     }
 }
