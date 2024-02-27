eb41c27162f4de0c0a8d1ac57a2bbce75bdb81e6
= <itzmohamed007@gmail.com>
2024-02-27 13:14:43 +0100

    enhanced authentication validation

--- src/main/java/com/aftas/aftasapi/services/imp/AuthenticationService.java
+++ src/main/java/com/aftas/aftasapi/services/imp/AuthenticationService.java
@@ -1,11 +1,16 @@
 package com.aftas.aftasapi.services.imp;
 
+import com.aftas.aftasapi.dtos.ResMember;
 import com.aftas.aftasapi.dtos.noRelations.LoginDto;
 import com.aftas.aftasapi.dtos.noRelations.RegisterDto;
+import com.aftas.aftasapi.enums.IdentityDocumentType;
+import com.aftas.aftasapi.exceptions.DocumentTypeViolationException;
+import com.aftas.aftasapi.exceptions.UniqueConstraintViolationException;
 import com.aftas.aftasapi.models.User;
 import com.aftas.aftasapi.repositories.UserRepository;
 import com.aftas.aftasapi.security.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,20 +28,26 @@ public class AuthenticationService {
     private final AuthenticationProvider authenticationProvider;
 
     public User register(RegisterDto registerDto) {
-        User userToCreate = User.builder()
+        try {
+            User userToCreate = User.builder()
                 .name(registerDto.getName())
                 .familyName(registerDto.getFamilyName())
                 .email(registerDto.getEmail())
                 .password(passwordEncoder.encode(registerDto.getPassword()))
                 .nationality(registerDto.getNationality())
                 .identityNumber(registerDto.getIdentityNumber())
-                .identityDocument(registerDto.getIdentityDocument())
+                .identityDocument(IdentityDocumentType.valueOf(registerDto.getIdentityDocument().toString()))
                 .accessionDate(LocalDate.now())
                 .role(registerDto.getRole())
                 .locked(true)
                 .build();
 
-        return userRepository.save(userToCreate);
+            return userRepository.save(userToCreate);
+        } catch (DataIntegrityViolationException e) {
+            throw new UniqueConstraintViolationException("Violated unique constraint (document number)");
+        } catch ( IllegalArgumentException e) {
+            throw new DocumentTypeViolationException("Violated available documents types [CIN, RESIDENCE_CARD, PASSPORT]");
+        }
     }
 
     public String login(LoginDto loginDto) {
